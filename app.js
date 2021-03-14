a059cf50373e94bddff5b951e16f5d23e774c37f
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-03-15 00:58:28 +0530

    ok

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
-app.use('/', indexRouter);
+app.use('/',httpsRedirect(true), indexRouter);
 
 //passport oauth 
 app.use(passport.initialize());
@@ -51,7 +51,7 @@ passport.deserializeUser(function(user, done) {
 passport.use (new GoogleStrategy({
   clientID: '96689537530-jkk11ojp0i4r1ffq7q6u8idamsm59c9j.apps.googleusercontent.com',
   clientSecret: 'NtXKC_Ba8lAWJGuysBU3ADXm',
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: "https://wissenaire.org/auth/google/callback",
   userProfileURL  : 'https://www.googleapis.com/oauth2/v3/userinfo'
 },
 function(accessToken, refreshToken, profile, done) {
