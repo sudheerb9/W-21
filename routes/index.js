ec2ce32f077fa8d047cbf75378b2b11018220c85
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-04-01 20:31:11 +0530

    ok

--- routes/index.js
+++ routes/index.js
@@ -670,18 +670,17 @@ router.get('/acd/test', function(req,res,next){
   
 })
 
-router.post('/acd/submission', function(req,res,next){
+router.post('/acd/submissiona', function(req,res,next){
   console.log('upload')
   const wissid = req.body.wissid;
-  const qno = req.body.question; 
-  var name = wissid + '-' + qno ;
+  var name = wissid + '-1' ;
   let file;
   let uploadPath;
   if (!req.files || Object.keys(req.files).length === 0) {
     console.log('not found')
     return res.send('No files were uploaded.');
   }
-  file = req.files.file;
+  file = req.files.filea;
   __dirname = path.dirname(__dirname) ;
   uploadPath = path.normalize( __dirname + '/public/uploads/' + name + '.pdf') 
   file.mv(uploadPath, function(err) {
@@ -693,5 +692,76 @@ router.post('/acd/submission', function(req,res,next){
     res.send('success');
   });
 })
+
+router.post('/acd/submissionb', function(req,res,next){
+  console.log('upload')
+  const wissid = req.body.wissid;
+  var name = wissid + '-2' ;
+  let file;
+  let uploadPath;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    console.log('not found')
+    return res.send('No files were uploaded.');
+  }
+  file = req.files.fileb;
+  __dirname = path.dirname(__dirname) ;
+  uploadPath = path.normalize( __dirname + '/public/uploads/' + name + '.pdf') 
+  file.mv(uploadPath, function(err) {
+    if (err){
+      console.log(err)
+      return res.send(err);
+    }
+    console.log('yaay ')
+    res.send('success');
+  });
+})
+
+
+router.post('/acd/submissionc', function(req,res,next){
+  console.log('upload')
+  const wissid = req.body.wissid;
+  var name = wissid + '-3' ;
+  let file;
+  let uploadPath;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    console.log('not found')
+    return res.send('No files were uploaded.');
+  }
+  file = req.files.filec;
+  __dirname = path.dirname(__dirname) ;
+  uploadPath = path.normalize( __dirname + '/public/uploads/' + name + '.pdf') 
+  file.mv(uploadPath, function(err) {
+    if (err){
+      console.log(err)
+      return res.send(err);
+    }
+    console.log('yaay ')
+    res.send('success');
+  });
+})
+
+router.post('/acd/submissiond', function(req,res,next){
+  console.log('upload')
+  const wissid = req.body.wissid;
+  var name = wissid + '-4' ;
+  let file;
+  let uploadPath;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    console.log('not found')
+    return res.send('No files were uploaded.');
+  }
+  file = req.files.filed;
+  __dirname = path.dirname(__dirname) ;
+  uploadPath = path.normalize( __dirname + '/public/uploads/' + name + '.pdf') 
+  file.mv(uploadPath, function(err) {
+    if (err){
+      console.log(err)
+      return res.send(err);
+    }
+    console.log('yaay ')
+    res.send('success');
+  });
+})
+
 
 module.exports = router;
